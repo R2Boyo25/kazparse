8816314eb3adc6b41bfb173e04e25a84ba71c5d5
R2boyo25 <r2boyo25@gmail.com>
2021-11-27 21:40:47 -0600

    Add setup.py and .avalon

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='CLIParse',
+    packages=find_packages(include=['CLIParse']),
+    version='1',
+    description='R2Boyo25\'s Argument Parser',
+    author='R2Boyo25'
+)

19f00359b8df3b92e1a8266eb71c945f5b13fc5a
R2boyo25 <r2boyo25@gmail.com>
2021-12-01 20:42:01 -0600

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='CLIParse',
     packages=find_packages(include=['CLIParse']),
-    version='1.1',
+    version='1.2',
     description='R2Boyo25\'s Argument Parser',
     author='R2Boyo25'
 )
